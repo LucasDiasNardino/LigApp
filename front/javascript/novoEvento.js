0976f76ba790e954ffa7fde861f47bf3d71500e0
lucas nardino <lucas.diasnardino@gmail.com>
2024-03-23 16:24:39 -0300

    qr first impl

--- front/javascript/novoEvento.js
+++ front/javascript/novoEvento.js
@@ -22,3 +22,57 @@ function atualizarSubmit() {
   // Habilita o botão se ambos os campos estiverem preenchidos, caso contrário, desabilita
   submitBut.disabled = !(nomePreenchido && matriculaPreenchido);
 }
+
+function generateQRCode(titulo) {
+    var GoogleChartAPI = 'https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl=';
+
+    var contentQRCode = GoogleChartAPI+titulo;
+
+    document.querySelector('#qrCode').src = contentQRCode;
+
+}
+
+var edicaoModal = new bootstrap.Modal(document.getElementById("modalQrCode"));
+
+edicaoModal.show();
+
+generateQRCode("habibi");
+
+console.log("Modal de edição criado");
+
+document.getElementById("submitEvento").addEventListener("click", function () {
+  var titulo = document.getElementById("tituloEvento").value;
+  var descricao = document.getElementById("descricaoEvento").value;
+
+  var dados = {
+    titulo: titulo,
+    descricao: descricao,
+  };
+
+
+
+  //alert("Dados submetidos:", dados);
+
+  //   fetch("http://localhost:8080/evento/cadastrar", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+
+  //     body: JSON.stringify(dados),
+  //   })
+  //     .then(function (response) {
+  //       if (response.ok) {
+  //         //alert("Resposta ok");
+  //         console.log("Resposta ok");
+  //         return response.text();
+  //       }
+
+  //     })
+  //     .then(function (text) {
+  //       console.log(text);
+  //     })
+  //     .catch(function (error) {
+  //       console.error(error);
+  //     });
+});
