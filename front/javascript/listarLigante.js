fb8ad6f6d6f973e12084960793afced7b5dc1ae0
lucas nardino <lucas.diasnardino@gmail.com>
2024-03-23 15:02:01 -0300

    fix modal + icons

--- front/javascript/listarLigante.js
+++ front/javascript/listarLigante.js
@@ -95,21 +95,46 @@ function listarLigantes() {
             <td>${ligante.matricula}</td>
             <td>${formatarData(ligante.dataCadastro)}</td>
             <td>
-                <button id="editar${tr.id}"type="button" class="btn btn-primary ml-auto" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Editar</button>
-                <button id="deletar${tr.id}" type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Deletar</button>
+                <button id="editar${tr.id}"type="button" class="btn btn-primary ml-auto" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
+                  </svg>
+                </button>
+                <button id="deletar${tr.id}" type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
+                  </svg>
+                </button>
             </td>
         `;
 
         console.log(tr.id);
         tbodyLigantes.appendChild(tr);
 
+
+
+
+        var edicaoModal = new bootstrap.Modal(
+          document.getElementById("modalEdicao")
+        );
+        edicaoModal.show();
+
+        console.log("Modal de edição criado");
+
+        
+
+
+
+
+
         document
           .getElementById(`deletar${tr.id}`)
           .addEventListener("click", function () {
             // Abre o modal de confirmação
             var confirmacaoModal = new bootstrap.Modal(
-              document.getElementById("staticBackdrop")
+              document.getElementById("modalConfirmarDelecao")
             );
+            console.log("Modal de confirmação criado com");
             confirmacaoModal.show();
 
             // Adiciona evento de clique ao botão de confirmação dentro do modal
@@ -119,6 +144,28 @@ function listarLigantes() {
                 deletarLigante(tr.id);
                 console.log("Item com ID", tr.id, "deletado!");
 
+                // Fecha o modal após a confirmação
+                confirmacaoModal.hide();
+
+                // Fecha o backdrop
+                document.querySelector(".modal-backdrop").remove();
+              });
+
+              // Adiciona evento de clique ao botão de confirmação dentro do modal
+            document
+            .getElementById("cancelarModal")
+            .addEventListener("click", function () {
+              // Fecha o modal após a confirmação
+              confirmacaoModal.hide();
+
+              // Fecha o backdrop
+              document.querySelector(".modal-backdrop").remove();
+            });
+
+            // Adiciona evento de clique ao botão de confirmação dentro do modal
+            document
+              .getElementById("fecharModal")
+              .addEventListener("click", function () {
                 // Fecha o modal após a confirmação
                 confirmacaoModal.hide();
 
