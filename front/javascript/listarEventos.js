0921791a8dd739b40e87a86a9bef06725ffed7b9
nardino <lucas.diasnardino@gmail.com>
2024-04-07 01:01:01 -0300

    tela listar eventos

--- front/javascript/listarEventos.js
+++ front/javascript/listarEventos.js
@@ -26,21 +26,6 @@ function formatarData(dataString) {
   return "Data Inválida";
 }
 
-function deletarLigante(id) {
-  fetch(`http://localhost:8080/ligante/deletar/${id}`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => {
-      console.log(response);
-      listarLigantes();
-    })
-    .catch((error) => {
-      console.error("Erro:", error);
-    });
-}
 
 function listarLigantes() {
   fetch("http://localhost:8080/evento/listar", {
@@ -57,7 +42,7 @@ function listarLigantes() {
         var tablePlaceholder = document.getElementById("tablePlaceholder");
         tablePlaceholder.innerHTML = `
             <div class="alert alert-danger" role="alert">
-                Não há ligantes cadastrados!
+                Não há eventos cadastrados!
             </div>
             `;
         return;
@@ -65,17 +50,16 @@ function listarLigantes() {
 
       var tablePlaceholder = document.getElementById("tablePlaceholder");
       tablePlaceholder.innerHTML = `
-        <table id="tabelaLigantes" class="table" style="text-align:center">
-                    <thead id="theadLigantes">
+        <table id="tabelaEventos" class="table" style="text-align:center">
+                    <thead id="theadEventos">
                       <tr>
-                        <th scope="col">Nome</th>
-                        <th scope="col">Matrícula</th>
-                        <th scope="col">Data de Admissão</th>
-                        <th scope="col">Ações</th>
+                        <th scope="col">Data</th>
+                        <th scope="col">Título</th>
+                        <th scope="col">Descrição</th>
+                        <th scope="col">Participantes</th>
                       </tr>
                     </thead>
-                    <tbody id="tbodyLigantes">
-                        
+                    <tbody id="tbodyEventos">
                     </tbody>
                 </table> 
     `;
@@ -84,49 +68,28 @@ function listarLigantes() {
 
       // criar as linhas da tabela
 
-      var tbodyLigantes = document.getElementById("tbodyLigantes");
+      var tbodyEventos = document.getElementById("tbodyEventos");
 
-      data.forEach((ligante) => {
-        console.log(ligante);
+      data.forEach((evento) => {
+        console.log(evento);
         var tr = document.createElement("tr");
-        tr.id = ligante.id;
+        tr.id = evento.id;
         tr.innerHTML = `
-            <td>${ligante.nome}</td>
-            <td>${ligante.matricula}</td>
-            <td>${formatarData(ligante.dataCadastro)}</td>
+            <td>${formatarData(evento.data)}</td>
+            <td>${evento.titulo}</td>
+            <td>${evento.descricao}</td>
             <td>
-                <button id="editar${tr.id}"type="button" class="btn btn-primary ml-auto" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
-                  </svg>
-                </button>
-                <button id="deletar${tr.id}" type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
-                  </svg>
-                </button>
+              <button type="button" class="btn" style="background-color:transparent">
+                ${evento.participantes.length}
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+                  <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4"/>
+                </svg>
+              </button>
             </td>
         `;
 
         console.log(tr.id);
-        tbodyLigantes.appendChild(tr);
-
-
-
-
-        // var edicaoModal = new bootstrap.Modal(
-        //   document.getElementById("modalEdicao")
-        // );
-
-        // //definindo placeholders
-        // inputPlaceholder = document.getElementById("inputNome");
-
-        // inputPlaceholder.value = ligante.nome;
-
-        // edicaoModal.show();
-
-        // console.log("Modal de edição criado");
-
+        tbodyEventos.appendChild(tr);
         
         document
           .getElementById(`deletar${tr.id}`)
